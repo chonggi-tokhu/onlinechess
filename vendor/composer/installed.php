a8cec6ff749d2f92b49e4343e38fda789425df85
chonggi-tokhu <120172454+chonggi-tokhu@users.noreply.github.com>
2023-04-23 18:37:40 +0900

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hehehe/hehehe',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hehehe/hehehe' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ryanhs/chess.php' => array(
+            'pretty_version' => 'v1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => '1efbef47e7b757957ff926d073606d6342e25445',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ryanhs/chess.php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
